ecc4f24c8b0b3bbd66afa4dbe2ec721c4f3c9b22
Changlong.Zang <zclongpop123@gmail.com>
2022-04-24 11:54:59 +0800

    Create install_deadline_client.cmd

---
+++ install_deadline_client.cmd
@@ -0,0 +1,2 @@
+DeadlineClient-10.1.17.4-windows-installer.exe  --mode unattended --repositorydir //10.10.10.7/DeadlineRepository10 --dbsslcertificate //10.10.10.7/DeadlineRepository10/Deadline10Client.pfx"
+pause
