2e715c273089a8761971a8d854b551f685ae1f28
Changlong.Zang <zclongpop123@gmail.com>
2022-04-24 11:58:36 +0800

    Create install_houdini.cmd

---
+++ install_houdini.cmd
@@ -0,0 +1,2 @@
+houdini-16.0.504.20-win64-vc14.exe /S /AcceptEula=yes /LicenseServer=Yes /DesktopIcon=No /FileAssociations=Yes /HoudiniServer=Yes /EngineUnity=Yes /EngineMaya=Yes /EngineUnreal=Yes /HQueueServer=No /HQueueClient=No /IndustryFileAssociations=Yes /InstallDir="\Houdini 16.0.504.20" /LicenseServer=Yes /ForceLicenseServer=Yes /MainApp=Yes /Registry=Yes
+pause
